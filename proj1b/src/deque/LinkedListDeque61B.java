d67768e022b30a82cf997a25a820a5faaa55fc32
MockCanine <2286596153@qq.com>
2026-03-24 16:29:43 +0800

    proj1b

---
+++ proj1b/src/deque/LinkedListDeque61B.java
@@ -0,0 +1,238 @@
+package deque;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class LinkedListDeque61B<T> implements Deque61B<T> {
+    private class LinkedListDequeIterator implements Iterator<T> {
+        private int idx;
+        public LinkedListDequeIterator() {
+            idx = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return idx < size;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                return null;
+            }
+            return get(idx++);
+        }
+    }
+
+    private class Node {
+        Node prev, next;
+        T item;
+        public Node(T item, Node prev, Node next) {
+            this.prev = prev;
+            this.next = next;
+            this.item = item;
+        }
+    }
+
+    int size;
+    Node sentinel;
+
+    public LinkedListDeque61B() {
+        size = 0;
+        /* Sentinel self reference -> invariants: any node.prev or node.next will
+           never be null, so no special cases for addFirst and addLast
+         */
+        sentinel = new Node(null, null, null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new LinkedListDequeIterator();
+    }
+
+    private boolean contains(T item) {
+        for (T i : this) {
+            // As a container, use the item's equals()
+            if (i.equals(item)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof LinkedListDeque61B other) {
+            if (this.size == other.size) {
+                for (T item : this) {
+                    if (!other.contains(item)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        List<String> listOfItems = new ArrayList<>();
+        for (T item : this) {
+            listOfItems.add(item.toString());
+        }
+        return "[" + String.join(", ", listOfItems) + "]";
+    }
+
+    /**
+     * Add {@code x} to the front of the deque. Assumes {@code x} is never null.
+     *
+     * @param x item to add
+     */
+    @Override
+    public void addFirst(T x) {
+        // Care about the four arrows that connect one node with its prev
+        // and next
+        Node f = new Node(x, sentinel, sentinel.next);
+        sentinel.next.prev = f;
+        sentinel.next = f;
+        size++;
+    }
+
+    /**
+     * Add {@code x} to the back of the deque. Assumes {@code x} is never null.
+     *
+     * @param x item to add
+     */
+    @Override
+    public void addLast(T x) {
+        Node l = new Node(x, sentinel.prev, sentinel);
+        sentinel.prev.next = l;
+        sentinel.prev = l;
+        size++;
+    }
+
+    /**
+     * Returns a List copy of the deque. Does not alter the deque.
+     *
+     * @return a new list copy of the deque.
+     */
+    @Override
+    public List<T> toList() {
+        List<T> returnList = new ArrayList<>();
+        // Invariants -> sentinel.next
+        Node current = sentinel.next;
+        for (int i = size; i > 0; i--) {
+            returnList.add(current.item);
+            current = current.next;
+        }
+        return returnList;
+    }
+
+    /**
+     * Returns if the deque is empty. Does not alter the deque.
+     *
+     * @return {@code true} if the deque has no elements, {@code false} otherwise.
+     */
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Returns the size of the deque. Does not alter the deque.
+     *
+     * @return the number of items in the deque.
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Remove and return the element at the front of the deque, if it exists.
+     *
+     * @return removed element, otherwise {@code null}.
+     */
+    @Override
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        Node first = sentinel.next;
+        sentinel.next = first.next;
+        sentinel.next.prev = sentinel;
+        size--;
+        return first.item;
+    }
+    /**
+     * Remove and return the element at the back of the deque, if it exists.
+     *
+     * @return removed element, otherwise {@code null}.
+     */
+    @Override
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        Node last = sentinel.prev;
+        sentinel.prev = last.prev;
+        sentinel.prev.next = sentinel;
+        size--;
+        return last.item;
+    }
+
+    /**
+     * The Deque61B abstract data type does not typically have a get method,
+     * but we've included this extra operation to provide you with some
+     * extra programming practice. Gets the element, iteratively. Returns
+     * null if index is out of bounds. Does not alter the deque.
+     *
+     * @param index index to get
+     * @return element at {@code index} in the deque
+     */
+    @Override
+    public T get(int index) {
+        if (index >= 0 && index < size) {
+            Node current = sentinel.next;
+            while (index != 0) {
+                current = current.next;
+                index--;
+            }
+            return current.item;
+        }
+        return null;
+    }
+
+    private T getRecursive(Node n, int idx) {
+        if (idx != 0) {
+            return getRecursive(n.next, --idx);
+        }
+        return n.item;
+    }
+    /**
+     * This method technically shouldn't be in the interface, but it's here
+     * to make testing nice. Gets an element, recursively. Returns null if
+     * index is out of bounds. Does not alter the deque.
+     *
+     * @param index index to get
+     * @return element at {@code index} in the deque
+     */
+    @Override
+    public T getRecursive(int index) {
+        if (index >= 0 && index < size) {
+            return getRecursive(sentinel.next, index);
+        }
+        return null;
+    }
+
+    static void main() {
+        Deque61B<Integer> lld = new LinkedListDeque61B<>();
+        lld.addLast(0);   // [0]
+        lld.addLast(1);   // [0, 1]
+        lld.addFirst(-1); // [-1, 0, 1]
+    }
+}
